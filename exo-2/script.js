13870174bc8fec2343f8bed70962fbd90dfbd33a
Jonathan <jonathan.courat@bag-era.fr>
2025-01-07 09:46:32 +0100

    exo 2 dates

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,23 @@
 // you can write js here
 console.log('exo-2');
+
+const isTesting = false; 
+
+let myDate = new Date();
+
+if (isTesting) {
+  // --
+  myDate = new Date("01/12/2025");
+}
+
+const notWE = "Non. c'est pad le WE"; 
+const yesWE = "C'est le WE";
+
+
+const currentDay = myDate.getDay();
+
+if (currentDay === 0 || currentDay === 6) {
+  console.log(yesWE);
+} else {
+  console.log(notWE);
+}
