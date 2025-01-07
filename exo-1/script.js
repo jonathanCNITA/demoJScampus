cf2e03cb4421f4aaf53bb228954341174929fdee
Jonathan <jonathan.courat@bag-era.fr>
2025-01-07 09:32:33 +0100

    exo 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,8 @@
 // you can write js here
-console.log('hello from file');
+console.log('hello from file 1');
+let kelvin = prompt("Quelle est la température en Kelvins aujourd'hui ?");;
+let celcius = kelvin - 273;
+
+let fahrenheits = Math.floor(celcius * (9/5) + 3);
+
+console.log('fahrenheits: ', fahrenheits);
